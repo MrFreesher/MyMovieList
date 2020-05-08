064d58503af066802ecee499bce34b069c5cb4b5
MrFreesher <artur260@op.pl>
2020-05-08 22:51:47 +0200

    Add register user

--- app/src/main/java/com/freesher/mymovielist/register/RegisterActivity.kt
+++ app/src/main/java/com/freesher/mymovielist/register/RegisterActivity.kt
@@ -1,26 +1,29 @@
 package com.freesher.mymovielist.register
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
+import com.freesher.mymovielist.HomeActivity
 import com.freesher.mymovielist.R
 import com.freesher.mymovielist.utils.toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
+        firebaseAuth = FirebaseAuth.getInstance()
         registerButton.setOnClickListener {
-            val email = emailEditText.text.trim()
-            val password = passwordEditText.text.trim()
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
 
             val isEmailValid = validateEmail(email)
             val isPasswordNotEmpty = checkIsPasswordNotEmpty(password)
             if (isEmailValid && isPasswordNotEmpty) {
-                toast("Everything is valid")
+                registerUser(email, password)
             }
 
         }
@@ -28,7 +31,7 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-    private fun validateEmail(email: CharSequence): Boolean {
+    private fun validateEmail(email: String): Boolean {
         return if (email.isEmpty()) {
             emailEditText.error = "Empty email"
             false
@@ -45,7 +48,7 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-    private fun checkIsPasswordNotEmpty(password: CharSequence): Boolean {
+    private fun checkIsPasswordNotEmpty(password: String): Boolean {
         return if (password.isEmpty()) {
             passwordEditText.error = "Empty password"
             false
@@ -53,6 +56,22 @@ class RegisterActivity : AppCompatActivity() {
             passwordEditText.error = null
             true
         }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@RegisterActivity, HomeActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    startActivity(intent)
+                } else {
+                    task.exception?.message?.let {
+                        toast(it)
+                    }
+                }
+            }
     }
 
 

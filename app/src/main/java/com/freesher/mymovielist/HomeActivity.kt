93f8c0033045c6b39d2bcb3c476847efc96f4174
MrFreesher <artur260@op.pl>
2020-05-09 23:44:25 +0200

    Add logout function to HomeActivity

--- app/src/main/java/com/freesher/mymovielist/HomeActivity.kt
+++ app/src/main/java/com/freesher/mymovielist/HomeActivity.kt
@@ -1,12 +1,34 @@
 package com.freesher.mymovielist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.freesher.mymovielist.utils.logout
+import com.google.firebase.auth.FirebaseAuth
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.signOut -> {
+                FirebaseAuth.getInstance().signOut()
+                logout()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/freesher/mymovielist/utils/Helper.kt
+++ app/src/main/java/com/freesher/mymovielist/utils/Helper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import com.freesher.mymovielist.HomeActivity
+import com.freesher.mymovielist.login.LoginActivity
 
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -11,6 +12,13 @@ fun Context.toast(message: String) {
 
 fun Context.login() {
     val intent = Intent(this, HomeActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    startActivity(intent)
+}
+
+fun Context.logout() {
+    val intent = Intent(this, LoginActivity::class.java).apply {
         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
     startActivity(intent)

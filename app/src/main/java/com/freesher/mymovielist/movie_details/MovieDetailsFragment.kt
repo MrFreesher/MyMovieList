f45085a8814e0f0fae392242df6bc8d8e18ad2ad
MrFreesher <artur260@op.pl>
2020-05-18 23:52:50 +0200

    Add fetching details data from database

--- app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsFragment.kt
@@ -5,19 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
 import com.freesher.mymovielist.R
+import com.freesher.mymovielist.data.Movie
+import kotlinx.android.synthetic.main.fragment_movie_details.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MovieDetailsFragment : Fragment() {
-
+    private val viewModel:MovieDetailsViewModel by viewModels()
+    val args: MovieDetailsFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.movieDetails.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                movieTitleContent.text = it.title
+                movieShortDescriptionContent.text = it.shortDescription
+                movieYearContent.text = it.year.toString()
+                progressBar2.visibility = View.GONE
+                movieDetails.visibility = View.VISIBLE
+            }
+        })
+
+        val id = args.movieId
+        viewModel.getDetailsAboutMovieById(id)
+        progressBar2.visibility = View.VISIBLE
+
     }
 
 }
--- app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsViewModel.kt
@@ -1,6 +1,43 @@
 package com.freesher.mymovielist.movie_details
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.freesher.mymovielist.data.Movie
+import com.freesher.mymovielist.utils.NODE_MOVIES
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class MovieDetailsViewModel : ViewModel(){
+class MovieDetailsViewModel : ViewModel() {
+
+    private val firebaseDatabase = FirebaseDatabase.getInstance()
+
+    private val _movieDetails = MutableLiveData<Movie>()
+    val movieDetails: LiveData<Movie>
+        get() = _movieDetails
+
+    fun getDetailsAboutMovieById(id: String) {
+        val moviesDatabase = firebaseDatabase.getReference("/$NODE_MOVIES/${id}")
+
+        moviesDatabase.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    val movie = snapshot.getValue(Movie::class.java)
+//                    val movie = snapshot.children.iterator().next()
+//                        .getValue(Movie::class.java)
+                    movie?.id = snapshot.key
+                    _movieDetails.value = movie
+                }
+            }
+
+        })
+    }
 }

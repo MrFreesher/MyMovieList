c951102916aad6b2900bd2f29b00c0e17bcd0bf0
MrFreesher <artur260@op.pl>
2020-05-20 16:13:25 +0200

    Add dialog to remove movie from favorites

--- app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/freesher/mymovielist/movie_details/MovieDetailsFragment.kt
@@ -2,31 +2,29 @@ package com.freesher.mymovielist.movie_details
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
-import androidx.navigation.ui.NavigationUI
 import com.freesher.mymovielist.HomeActivity
 import com.freesher.mymovielist.R
 import com.freesher.mymovielist.data.Movie
-import com.freesher.mymovielist.movie_list.MovieListFragmentDirections
-import com.google.android.material.internal.NavigationMenu
+import com.freesher.mymovielist.utils.toast
 import kotlinx.android.synthetic.main.fragment_movie_details.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MovieDetailsFragment : Fragment() {
-    private val viewModel:MovieDetailsViewModel by viewModels()
+    private val viewModel: MovieDetailsViewModel by viewModels()
     val args: MovieDetailsFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       setHasOptionsMenu(true)
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_movie_details, container, false)
     }
 
@@ -56,19 +54,37 @@ class MovieDetailsFragment : Fragment() {
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         super.onPrepareOptionsMenu(menu)
-        if(viewModel.isFavorite.value == false){
+        if (viewModel.isFavorite.value == false) {
             menu.findItem(R.id.favorite).isVisible = false
             menu.findItem(R.id.nonfavorite).isVisible = true
 
-        }else{
+        } else {
             menu.findItem(R.id.favorite).isVisible = true
             menu.findItem(R.id.nonfavorite).isVisible = false
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.favorite -> { viewModel.removeFromFavorites()}
+        when (item.itemId) {
+            R.id.favorite -> {
+                AlertDialog.Builder(requireContext()).also {
+                    it.setTitle("Remove from favorites")
+                    it.setMessage("Are you sure to remove this movie from your list")
+                    it.setPositiveButton(
+                        "Yes"
+                    ) { dialog, _ ->
+
+
+                        dialog.dismiss()
+                        progressBar2.visibility = View.VISIBLE
+                        viewModel.removeFromFavorites()
+                        progressBar2.visibility = View.GONE
+                        requireContext().toast("Movie was removed from your list")
+                    }
+                    it.setNegativeButton("No") { dialog, _ -> dialog.dismiss() }
+                }.show()
+
+            }
             R.id.nonfavorite -> {
                 val movie = Movie().apply {
                     id = viewModel.movieDetails.value?.id
@@ -82,17 +98,19 @@ class MovieDetailsFragment : Fragment() {
 
 
 
-
-                viewModel.addToFavorites(movie)}
-            android.R.id.home -> {Navigation.findNavController(activity as HomeActivity,R.id.fragment).popBackStack()}
+                progressBar2.visibility = View.VISIBLE
+                viewModel.addToFavorites(movie)
+                progressBar2.visibility = View.GONE
+                requireContext().toast("Movie was added to your list")
+            }
+            android.R.id.home -> {
+                Navigation.findNavController(activity as HomeActivity, R.id.fragment).popBackStack()
+            }
 
         }
 
         return true
     }
-
-
-
 
 
 }
